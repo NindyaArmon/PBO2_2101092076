4b0c88b5c20d68b4a009251bbae65eb6abe4cc6f
Nindya Armon <nindyamori516@gmail.com>
2023-04-15 06:42:24 +0700

    Update AnggotaController.java

--- src/nindy/controller/AnggotaController.java
+++ src/nindy/controller/AnggotaController.java
@@ -7,9 +7,11 @@ package nindy.controller;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import nindy.dao.AnggotaDao;
 import nindy.dao.AnggotaDaoImpl;
 import nindy.dao.FormAnggota;
@@ -43,21 +45,73 @@ public class AnggotaController {
     public void clearForm(){
         formAnggota.getTxtKodeAnggota().setText("");
         formAnggota.getTxtNamaAnggota().setText("");
+        formAnggota.getTxtAlamat().setText("");
     }
     
     public void insert(){
         try {
             anggota = new Anggota();
-            anggota.setKodeAnggota(
-                    formAnggota.getTxtKodeAnggota().getText());
-            anggota.setNamaAnggota(
-                    formAnggota.getTxtNamaAnggota().getText());
-            anggota.setAlamat(
-                    formAnggota.getTxtAlamat().getText());
-            anggota.setJenisKelamin(
-                    formAnggota.getCboJenisKelamin().getSelectedItem.toString());
+            anggota.setKodeAnggota(formAnggota.getTxtKodeAnggota().getText());
+            anggota.setNamaAnggota(formAnggota.getTxtNamaAnggota().getText());
+            anggota.setAlamat(formAnggota.getTxtAlamat().getText());
+            anggota.setJenisKelamin(formAnggota.getCboJenisKelamin().getSelectedItem().toString());
             anggotaDao.insert(con, anggota);
             JOptionPane.showMessageDialog(formAnggota, "Entri OK");
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void update(){
+        try {
+            anggota = new Anggota();
+            anggota.setKodeAnggota(formAnggota.getTxtKodeAnggota().getText());
+            anggota.setNamaAnggota(formAnggota.getTxtNamaAnggota().getText());
+            anggota.setAlamat(formAnggota.getTxtAlamat().getText());
+            anggota.setJenisKelamin(formAnggota.getCboJenisKelamin().getSelectedItem().toString());
+            anggotaDao.update(con, anggota);
+            JOptionPane.showMessageDialog(formAnggota, "Update OK");
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void delete(){
+        try {
+            anggotaDao.delete(con, anggota);
+            JOptionPane.showMessageDialog(formAnggota, "Delete OK");
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void cari(){
+        try {
+            String kode = formAnggota.getTxtKodeAnggota().getText();
+            anggota = anggotaDao.getAnggota(con, kode);
+            if (anggota != null){
+                anggota.setNamaAnggota(formAnggota.getTxtNamaAnggota().getText());
+                anggota.setAlamat(formAnggota.getTxtAlamat().getText());
+                anggota.setJenisKelamin(formAnggota.getCboJenisKelamin().getSelectedItem().toString());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void tampil(){
+        try {
+            DefaultTableModel tabel = (DefaultTableModel) formAnggota.getTblAnggota().getModel();
+            tabel.setRowCount(0);
+            List<Anggota> list = anggotaDao.getAllAnggota(con);
+            for (Anggota anggota1 : list) {
+                Object[] row = {
+                    anggota1.getKodeAnggota(),
+                    anggota1.getNamaAnggota(),
+                    anggota1.getJenisKelamin()
+                };
+                tabel.addRow(row); 
+            }
         } catch (Exception ex) {
             Logger.getLogger(AnggotaController.class.getName()).log(Level.SEVERE, null, ex);
         }
