2425bf505c2f037c443eb442df7023cf1835f8df
Nindya Armon <nindyamori516@gmail.com>
2023-06-07 12:42:09 +0700

    Update BukuDaoImpl.java

--- src/nindy/dao/BukuDaoImpl.java
+++ src/nindy/dao/BukuDaoImpl.java
@@ -20,40 +20,39 @@ public class BukuDaoImpl implements BukuDao{
 
     @Override
     public void insert(Connection con, Buku buku) throws Exception {
-        String sql = "insert into buku values(?,?,?,?)";
+        String sql = "insert into buku value(?,?,?,?)";
         PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, buku.getKodebuku());
-        ps.setString(2, buku.getJudulbuku());
+        ps.setString(2, buku.getJudul());
         ps.setString(3, buku.getPengarang());
         ps.setString(4, buku.getPenerbit());
         ps.executeUpdate();
+        
     }
-    
- 
+
     @Override
-    public void update(Connection con,Buku buku) throws Exception {
-        String sql = "update buku set judulbuku=?, pengarang=?, penerbit=? where kodebuku=?";
+    public void update(Connection con, Buku buku) throws Exception {
+        String sql = "Update buku set judul=?, pengarang=?, penerbit=? where kodebuku = ?";
         PreparedStatement ps = con.prepareStatement(sql);
-        ps.setString(1, buku.getJudulbuku());
+        ps.setString(1, buku.getJudul());
         ps.setString(2, buku.getPengarang());
         ps.setString(3, buku.getPenerbit());
-         ps.setString(4, buku.getKodebuku());
+        ps.setString(4, buku.getKodebuku());
         ps.executeUpdate();
+        
     }
-    
 
     @Override
     public void delete(Connection con, Buku buku) throws Exception {
-        String sql = "delete from buku where kodebuku=?";
+        String sql = "delete from buku " + "where kodebuku = ?";
         PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, buku.getKodebuku());
-        System.err.println("Kode   "+buku.getKodebuku());
         ps.executeUpdate();
     }
-    
+
     @Override
-      public Buku getBuku(Connection con,String kode) throws Exception {
-        String sql = "select * from buku where kodebuku=?";
+    public Buku getBuku(Connection con, String kode) throws Exception {
+        String sql = "select * from buku " + "where kodebuku = ?";
         PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, kode);
         ResultSet rs = ps.executeQuery();
@@ -61,31 +60,28 @@ public class BukuDaoImpl implements BukuDao{
         if (rs.next()){
             buku = new Buku();
             buku.setKodebuku(rs.getString(1));
-            buku.setJudulbuku(rs.getString(2));
+            buku.setJudul(rs.getString(2));
             buku.setPengarang(rs.getString(3));
             buku.setPenerbit(rs.getString(4));
         }
-         return buku;
+        return buku;    
     }
-      
-      
+
     @Override
-      public List<Buku> getAllBuku(Connection con) throws Exception {
-        String sql = "select * from buku";
-        PreparedStatement ps = con.prepareCall(sql);
+    public List<Buku> getAllBuku(Connection con) throws Exception {
+       String sql = "select * from buku";
+        PreparedStatement ps = con.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
         List<Buku> list = new ArrayList<>();
         Buku buku = null;
         while (rs.next()){
-        buku = new Buku();
-        buku.setKodebuku(rs.getString(1));
-        buku.setJudulbuku(rs.getString(2));
+            buku = new Buku();
+            buku.setKodebuku(rs.getString(1));
+            buku.setJudul(rs.getString(2));
             buku.setPengarang(rs.getString(3));
             buku.setPenerbit(rs.getString(4));
-            list.add(buku);
+            list.add(buku);  
         }
-         return list;
-   
+        return list;
     }
-
 }
